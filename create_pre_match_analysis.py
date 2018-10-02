03df828c6a35389fc0ab89f162668ad792d3814f
carljonsson <qWe12345>
2018-10-02 17:18:44 +0200

    dont fuck up

--- create_pre_match_analysis.py
+++ create_pre_match_analysis.py
@@ -3,8 +3,12 @@ conn = sqlite3.connect('hockeystats.db')
 c = conn.cursor()
 
 from pre_match_functions import get_form
+from pre_match_functions import getOdds1X2
+from pre_match_functions import getOdds55
 from pre_match_functions import get_player_form
 from pre_match_functions import get_team_schedule
+from pre_match_functions import get_offence_info
+from calcFunctions import calculate_team_strength
 
 def create_pre_match_analysis(gamedate, serie, hometeam, awayteam):
 
@@ -12,16 +16,46 @@ def create_pre_match_analysis(gamedate, serie, hometeam, awayteam):
     if int(gamedate[5:7]) > 6:
         seasonYear += 1
 
-    [form, offForm, defForm, points5, goals5, conc5, points1, goals1, conc1] = get_form(hometeam,seasonYear,gamedate,c)
-    print([form, offForm, defForm, points5, goals5, conc5, points1, goals1, conc1])
 
-    [form, offForm, defForm, points5, goals5, conc5, points1, goals1, conc1] = get_form(awayteam, seasonYear,gamedate,c)
-    print([form, offForm, defForm, points5, goals5, conc5, points1, goals1, conc1])
+    [form, offForm1, defForm1, points5, hgoals5, conc5, points3, hgoals3, hgoals1, points1, hgoals1, conc1] = get_form(hometeam,seasonYear,gamedate,c)
+    print(hgoals1)
+    [form, offForm2, defForm2, points5, agoals5, conc5, points3, agoals3, hgoals1, points1, agoals1, conc1] = get_form(awayteam, seasonYear,gamedate,c)
+    #Offence
+    off_info = get_offence_info(hometeam, offForm1, hgoals5, hgoals3, hgoals1)
+    #Defence
+    #Goalie
+    #Powerplay
+    #Boxplay
+    #Overall
+
+
+    c.execute("SELECT gamedate from teamgames where team = ? order by gamedate DESC",[hometeam])
+    htd = c.fetchall()
+    gamedate_pre_h = htd[0][0]
+
+    c.execute("SELECT gamedate from teamgames where team = ? order by gamedate DESC", [awayteam])
+    atd = c.fetchall()
+    gamedate_pre_a = atd[0][0]
+
+    [home_score, home_points, home_season_points, home_player_score_final] = calculate_team_strength(hometeam, gamedate_pre_h, c)
+    [away_score, away_points, away_season_points, away_player_score_final] = calculate_team_strength(awayteam, gamedate_pre_a, c)
+
+    [odds1, oddsX, odds2] = getOdds1X2(home_score,away_score)
+
+    [prob4, prob5, prob6] = getOdds55(offForm1, defForm1, offForm2, defForm2)
+
 
     get_player_form(hometeam, seasonYear, gamedate, c)
 
-    print(get_team_schedule(hometeam, seasonYear, gamedate, c))
-    print(get_team_schedule(awayteam, seasonYear, gamedate, c))
+    #Goal scorers
+    #Målvakt
+    #Bäst form
+    #Svag form
+    #Största stjärnor
+    #To score
+
+    schedule_home = (get_team_schedule(hometeam, seasonYear, gamedate, c))
+    schedule_away = (get_team_schedule(awayteam, seasonYear, gamedate, c))
 
 
 
@@ -37,3 +71,41 @@ def create_pre_match_analysis(gamedate, serie, hometeam, awayteam):
 
 
 create_pre_match_analysis('2018-10-02','SHL','Linköping HC','Brynäs IF')
+create_pre_match_analysis('2018-10-02','SHL','Skellefteå AIK','Växjö Lakers HC')
+create_pre_match_analysis('2018-10-02','SHL','IF Malmö Redhawks','Frölunda HC')
+
+if 1 == 2:
+
+    #TEST DATA ON TWO SEASONS
+
+    c.execute("SELECT GAMEID, SEASONID, SERIE FROM SCHEDULE WHERE (SEASONID = ? OR SEASONID = ?) AND SERIE = ?",[2017,2018,'SHL'])
+    testGames = c.fetchall()
+
+    for i in range(0, len(testGames)):
+        c.execute("SELECT GAMEID, HOMETEAM, AWAYTEAM, HSCORE1 + HSCORE2 + HSCORE3 as GOALS1, ASCORE1 + ASCORE2 + ASCORE3 as GOALS2 FROM STATS WHERE GAMEID = ?",[testGames[i][0]])
+        scoreStats = c.fetchall()
+
+        if scoreStats[0][3] > scoreStats[0][4]:
+            outcome = 1
+        elif scoreStats[0][3] < scoreStats[0][4]:
+            outcome = 3
+        else:
+            outcome = 2
+
+        tscore = scoreStats[0][3] + scoreStats[0][4]
+
+        c.execute("SELECT SCORE, FORM_SCORE, LAST_SEASONS_SCORE, PLAYER_SCORE FROM TEAMSCORE WHERE GAMEID = ? AND TEAM = ?",[testGames[i][0],scoreStats[0][1]])
+        hscore = c.fetchall()
+        c.execute("SELECT SCORE, FORM_SCORE, LAST_SEASONS_SCORE, PLAYER_SCORE FROM TEAMSCORE WHERE GAMEID = ? AND TEAM = ?",[testGames[i][0], scoreStats[0][2]])
+        ascore = c.fetchall()
+
+
+        c.execute("SELECT GAMEID FROM TEST_DATA_GAME WHERE GAMEID = ?",[testGames[i][0]])
+        chk = c.fetchall()
+
+        if len(chk) > 0:
+            pass
+        else:
+            c.execute("INSERT INTO TEST_DATA_GAME (GAMEID, OUTCOME, HGOALS, AGOALS, TGOALS, HSCORE, HFORM, HLAST, HPLAYER, ASCORE, AFORM, ALAST, APLAYER) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",[testGames[i][0],outcome, scoreStats[0][3], scoreStats[0][4], tscore, hscore[0][0], hscore[0][1], hscore[0][2], hscore[0][3], ascore[0][0], ascore[0][1], ascore[0][2], ascore[0][3]])
+
+        conn.commit()
--- pre_match_functions.py
+++ pre_match_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from datetime import date
+from scipy.stats import poisson
 
 def get_form(team,seasonYear,gamedate,c):
     c.execute("SELECT GAMEDATE, HOMEAWAY, OUTCOME, SCORE11 + SCORE12 + SCORE13 + SCORE14 as SCORE1, SCORE21 + SCORE22 + SCORE23 + SCORE24 as SCORE2, SHOTS1, SHOTS2, PENALTY1, PENALTY2, OPP_SCORE_SIMPLE FROM TEAMGAMES WHERE TEAM = ? and SEASONID = ? AND GAMEDATE < ? ORDER BY GAMEID DESC", [team, seasonYear, gamedate])
@@ -11,6 +12,9 @@ def get_form(team,seasonYear,gamedate,c):
     points5 = 0
     goals5 = 0
     conc5 = 0
+    points3 = 0
+    goals3 = 0
+    conc3 = 0
     points1 = 0
     goals1 = 0
     conc1 = 0
@@ -43,9 +47,14 @@ def get_form(team,seasonYear,gamedate,c):
         if i == 0:
             points1 += (4 - int(tg[i][2]))
             goals1 += int(tg[i][3])
-            conc5 += int(tg[i][4])
+            conc1 += int(tg[i][4])
 
-    return [form, offForm, defForm, points5, goals5, conc5, points1, goals1, conc1]
+        if i < 3:
+            points3 += (4 - int(tg[i][2]))
+            goals3 += int(tg[i][3])
+            conc3 += int(tg[i][4])
+
+    return [form, offForm, defForm, points5, goals5, conc5, points3, goals3, conc3, points1, goals1, conc1]
 
 def get_player_form(team,seasonYear,gamedate,c):
     c.execute("SELECT FORNAME, SURNAME, SUM(SCORE)/COUNT(SCORE) AS SCORE, SUM(OFFSCORE)/COUNT(OFFSCORE) AS OFFSCORE, SUM(DEFSCORE)/COUNT(DEFSCORE) AS DEFSCORE, SUM(GOALS) as GOALS, SUM(ASSISTS) as ASSISTS, COUNT(GAMEID) as GAMES FROM LINEUPS WHERE TEAM = ? and SEASONID = ? AND GAMEDATE < ? GROUP BY FORNAME, SURNAME, PERSONNR ORDER BY SCORE DESC",[team,seasonYear,gamedate])
@@ -67,3 +76,105 @@ def get_team_schedule(team, seasonYear, gamedate, c):
            schedule += 1/(d1-d2).days
 
     return schedule
+
+def getOdds1X2(homeScore,awayScore):
+    vect1 = [0.21, 0.245, 0.305, 0.365, 0.425, 0.485, 0.545, 0.635, 0.705, 0.77, 0.85]
+    vect2 = [0.7, 0.65, 0.55, 0.44, 0.35, 0.28, 0.22, 0.15, 0.12, 0.10, 0.06]
+    vectX = [0.09, 0.105, 0.145, 0.195, 0.225, 0.235, 0.235, 0.215, 0.175, 0.13, 0.09]
+
+    vectB = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2]
+
+    ratio = homeScore / awayScore
+
+    if ratio < 0.2:
+        ratio = 0.2
+
+    if ratio > 2.2:
+        ratio = 2.2
+
+    for i in range(0,10):
+        if ratio >= vectB[i] and ratio <= vectB[i+1]:
+
+            fact = (ratio-vectB[i])/(vectB[i+1]-vectB[i])
+
+            odds1 = vect1[i+1]*fact+vect1[i]*(1-fact)
+            oddsX = vectX[i+1]*fact+vectX[i]*(1-fact)
+            odds2 = vect2[i+1]*fact+vect2[i]*(1-fact)
+
+    return [odds1, oddsX, odds2]
+
+def getOdds55(offForm1, defForm1, offForm2, defForm2):
+
+    exp_score = offForm1/2 + offForm2/2 + defForm1/2 + defForm2/2
+
+    prob4 = poisson.cdf(4, exp_score)
+    prob5 = poisson.cdf(4, exp_score)
+    prob6 = poisson.cdf(4, exp_score)
+
+    return [prob4, prob5, prob6]
+
+def get_offence_info(team, offForm, goals5, goals3, goals1):
+
+    line1=""
+    line2=""
+
+    if offForm > 3.5:
+        line1 = team + " har öst in mål senaste matcherna."
+        line1 = team + " har bra fart på målskyttet."
+
+        if goals1 == 0:
+            line2 = "Senast blev man dock nollade mot okänd motståndare"
+        elif goals1 == 1:
+            line2 = "Dock bara ett mål senast mot okänd motståndare"
+        elif goals1 > 1 and goals1 < 5:
+            line2 = str(goals1) + " mål senast mot okänd motståndare"
+        else:
+            line2 = "Hela " + str(goals1) + " mål senast mot okänd motståndare"
+
+
+    elif offForm > 3:
+        line1 = "Stark offensiv för " + team + "."
+        line1 = team + " har bra fart på målskyttet."
+
+        if goals1 == 0:
+            line2 = "Senast blev man dock nollade mot okänd motståndare"
+        elif goals1 == 1:
+            line2 = "Dock bara ett mål senast mot okänd motståndare"
+        elif goals1 > 1 and goals1 < 5:
+            line2 = str(goals1) + " mål senast mot okänd motståndare"
+        else:
+            line2 = "Hela " + str(goals1) + " mål senast mot okänd motståndare"
+
+
+    elif offForm > 2.5:
+        line1 = "Offensiven ser hyfsad ut för " + team + ""
+
+        if goals1 == 0:
+            line2 = "Nollade senast mot okänd motståndare."
+        elif goals1 == 1:
+            line2 = "Bara ett mål senast mot okänd motståndare."
+        elif goals1 > 1 and goals1 < 5:
+            line2 = str(goals1) + " mål senast mot okänd motståndare"
+        else:
+            line2 = "Hela " + str(goals1) + " mål senast mot okänd motståndare"
+
+
+    elif offForm > 2.0:
+        line1 = team + " har problem med målskyttet."
+        line1 = team + " har inte fått offensiven att fungera."
+
+        if goals1 == 0:
+            line2 = "Nollade senast mot okänd motståndare."
+        elif goals1 == 1:
+            line2 = "Bara ett mål senast mot okänd motståndare."
+        elif goals1 > 2:
+            line2 = str(goals1) + " mål senast mot okänd motståndare var dock ett steg i rätt riktning."
+        else:
+            line2 = ""
+
+
+    print(line1, line2)
+
+    return line1
+
+pass
